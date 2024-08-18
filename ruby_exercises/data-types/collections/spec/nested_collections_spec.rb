06ae134214ecea0dd61dff862a7a5c88ef142837
Wally Wallace <jrvw001@gmail.com>
2024-08-18 13:27:25 -0600

    Complete some nested collections exercises

--- ruby_exercises/data-types/collections/spec/nested_collections_spec.rb
+++ ruby_exercises/data-types/collections/spec/nested_collections_spec.rb
@@ -8,46 +8,46 @@ RSpec.describe 'Nested Collections' do
     expect(second).to eq([87, 2])
   end
 
-  xit 'test 2' do
+  it 'test 2' do
     coordinates = [[2,5],[87,2],[56,39],[3,46]]
     # Using the coordinates variable defined above
     # Retrive the value 39
-    thirty_nine = _________
+    thirty_nine = coordinates[2][1]
     expect(thirty_nine).to eq(39)
   end
 
-  xit 'test 3' do
+  it 'test 3' do
     coordinates = [[2,5],[87,2],[56,39],[3,46]]
     # Using the coordinates variable defined above
     # set the last coordinates to [6, 55]
-    _________
+    coordinates[3] = [6,55]
 
     expected = [[2,5],[87,2],[56,39],[6,55]]
     expect(coordinates).to eq(expected)
   end
 
-  xit 'test 4' do
+  it 'test 4' do
     coordinates = [[2,5],[87,2],[56,39],[3,46]]
     # Using the coordinates variable defined above
     # set the second element of the last coordinates
     # to 0
-    ________
+    coordinates[3][1] = 0
 
     expected = [[2,5],[87,2],[56,39],[3, 0]]
     expect(coordinates).to eq(expected)
   end
 
-  xit 'test 5' do
+  it 'test 5' do
     coordinates = [[2,5],[87,2],[56,39],[3,46]]
     # Using the coordinates variable defined above
     # add the coordinate [4, 14]
-    _________
+    coordinates.append([4,14])
 
     expected = [[2,5],[87,2],[56,39],[3,46],[4, 14]]
     expect(coordinates).to eq(expected)
   end
 
-  xit 'test 6' do
+  it 'test 6' do
     team = {
       pitchers: ["Kenny", "Joan", "Shabaz"],
       fielders: ["Luke", "Chris", "Megan", "Mark", "Mackenzie"],
@@ -55,12 +55,12 @@ RSpec.describe 'Nested Collections' do
     }
     # Using the team variable defined above
     # retrieve all of the pitchers
-    pitchers = _________
+    pitchers = team[:pitchers]
     expected = ["Kenny", "Joan", "Shabaz"]
     expect(pitchers).to eq(expected)
   end
 
-  xit 'test 7' do
+  it 'test 7' do
     team = {
       pitchers: ["Kenny", "Joan", "Shabaz"],
       fielders: ["Luke", "Chris", "Megan", "Mark", "Mackenzie"],
@@ -68,7 +68,7 @@ RSpec.describe 'Nested Collections' do
     }
     # Using the team variable defined above
     # add "Phil" as a pitcher
-    __________
+    team[:pitchers].append("Phil")
 
     expected = {
       pitchers: ["Kenny", "Joan", "Shabaz", "Phil"],
@@ -78,7 +78,7 @@ RSpec.describe 'Nested Collections' do
     expect(team).to eq(expected)
   end
 
-  xit 'test 8' do
+  it 'test 8' do
     team = {
       pitchers: ["Kenny", "Joan", "Shabaz"],
       fielders: ["Luke", "Chris", "Megan", "Mark", "Mackenzie"],
@@ -87,7 +87,7 @@ RSpec.describe 'Nested Collections' do
     # Using the team variable defined above
     # create a new key :coaches with the value
     # of an empty array
-    _________
+    team.store(:coaches,[])
 
     expected = {
       pitchers: ["Kenny", "Joan", "Shabaz"],
@@ -98,7 +98,7 @@ RSpec.describe 'Nested Collections' do
     expect(team).to eq(expected)
   end
 
-  xit 'test 9' do
+  it 'test 9' do
     team = {
       pitchers: ["Kenny", "Joan", "Shabaz"],
       fielders: ["Luke", "Chris", "Megan", "Mark", "Mackenzie"],
@@ -106,11 +106,11 @@ RSpec.describe 'Nested Collections' do
     }
     # Using the team variable defined above
     # Find out how many fielders there are
-    num_fielders = ________
+    num_fielders = team[:fielders].length
     expect(num_fielders).to eq(5)
   end
 
-  xit 'test 10' do
+  it 'test 10' do
     team = {
       pitchers: ["Kenny", "Joan", "Shabaz"],
       fielders: ["Luke", "Chris", "Megan", "Mark", "Mackenzie"],
@@ -118,11 +118,11 @@ RSpec.describe 'Nested Collections' do
     }
     # Using the team variable defined above
     # Find out if "Kenny" is a pitcher
-    kenny_is_pitcher = ________
+    kenny_is_pitcher = team[:pitchers].include?"Kenny"
     expect(kenny_is_pitcher).to eq(true)
   end
 
-  xit 'test 11' do
+  it 'test 11' do
     # HINT: You may find it valuable to break the three_day_forecast variable
     # into multiple lines to make it more readable
 
